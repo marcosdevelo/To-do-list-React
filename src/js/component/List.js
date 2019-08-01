9ec06ebea04230b7955ac52d226395055cc7bf9c
marcosdevelo <marcosgomezda@gmail.com>
2019-08-01 00:10:18 +0000

    almost

--- src/js/component/List.js
+++ src/js/component/List.js
@@ -1,27 +1,40 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class List extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { value: "" };
+		this.state = {
+			newItem: "",
+			list: []
+		};
+	}
+	updateInput(key, value) {
+		this.setState({
+			[key]: value
+		});
+	}
+	addItem() {
+		const newItem = {
+			id: 1 + Math.random(),
+			value: this.state.newItem.slice()
+		};
 
-		this.handleChange = this.handleChange.bind(this);
-		this.keyPress = this.keyPress.bind(this);
+		const list = [...this.state.list];
+		list.push(newItem);
+
+		this.setState({
+			list,
+			newItem: ""
+		});
 	}
 
-	handleChange(e) {
-		this.setState({ value: e.target.value });
-	}
-
-	keyPress(e) {
-		if (e.keyCode == 13) {
-			console.log("value", e.target.value);
-			// put the login here
-		}
+	deleteItem(id) {
+		const list = [...this.state.list];
+		const updatedList = list.filter(item => item.id !== id);
+		this.setState({ list: updatedList });
 	}
 
 	render() {
@@ -31,30 +44,24 @@ export class List extends React.Component {
 					id="addToDo"
 					type="text"
 					placeholder="What needs to be done?"
-					value={this.state.value}
-					onKeyDown={this.keyPress}
-					onChange={this.handleChange}
-					fullWidth={true}
+					value={this.state.newItem}
+					onChange={e => this.updateInput("newItem", e.target.value)}
 				/>
+
+				<button onClick={() => this.addItem()}> Add </button>
+				<br />
+
 				<ul>
-					<li>
-						Wash My Hands
-						<span>
-							<i className="fas fa-times" />
-						</span>{" "}
-					</li>
-					<li>
-						Make the Bed
-						<span>
-							<i className="fas fa-times" />
-						</span>{" "}
-					</li>
-					<li>
-						{this.state.value}
-						<span>
-							<i className="fas fa-times" />
-						</span>{" "}
-					</li>
+					{this.state.list.map(item => {
+						return (
+							<li key={item.id}>
+								{item.value}
+								<span onClick={() => this.deleteItem(item.id)}>
+									<i className="fas fa-times" />
+								</span>
+							</li>
+						);
+					})}
 				</ul>
 			</div>
 		);
